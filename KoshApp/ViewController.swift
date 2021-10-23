0e761caa0af5a2e4bc1278bcb6d1cda1cc14a390
Aleksey Sivaks <koshmariel@gmail.com>
2021-10-23 15:13:40 +0400

    Button fixed Sound added

--- KoshApp/ViewController.swift
+++ KoshApp/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import AVFoundation //play sounds
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+
+
+    @IBAction func startPressed(_ sender: UIButton) {
+        print("START pressed")
+        playSound(fileName: "C")
     }
-
-
+    
+    func playSound(fileName: String) {
+        let url = Bundle.main.url(forResource: fileName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        print("\(fileName) played")
+    }
+    
+    /*
+    //Complex function
+    var player: AVAudioPlayer?
+    func playSound() {
+                    guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else {return}
+                                //project resources              name              extension
+                    do {
+                        try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                        //                                              .ambient for usage with sound off
+                        try AVAudioSession.sharedInstance().setActive(true)
+                        player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+                        guard let player = player else {return}
+                        player.play()
+                        } catch let error { print(error.localizedDescription) }
+        
+    }
+    */
+    
+    
 }
 
